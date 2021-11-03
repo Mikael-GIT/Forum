97aa223a8a8734f127248d13c049fde4f2d3938d
Mikael Tavares <mikael.tavares@unigranrio.br>
2021-11-03 13:41:58 -0300

    Adicionando busca por Id do topico com dto

--- src/main/java/br/com/alura/forum/Controllers/TopicosController.java
+++ src/main/java/br/com/alura/forum/Controllers/TopicosController.java
@@ -5,10 +5,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.models.Curso;
 import br.com.alura.forum.models.Topico;
+import br.com.alura.forum.models.dtos.DetalhesDoTopicoDto;
 import br.com.alura.forum.models.dtos.TopicoDto;
 import br.com.alura.forum.models.forms.TopicoForm;
 import br.com.alura.forum.repositories.TopicoRepository;
@@ -36,6 +39,11 @@ public class TopicosController {
             return TopicoDto.converter(topicos);
         }
         return TopicoDto.converter(topicoService.listarPorNomeCurso(nomeCurso));
+    }
+
+    @GetMapping("/{id}")
+    public DetalhesDoTopicoDto detalhar(@PathVariable Long id) {
+        return topicoService.detalhar(id);
     }
 
     @PostMapping
--- src/main/java/br/com/alura/forum/services/TopicoService.java
+++ src/main/java/br/com/alura/forum/services/TopicoService.java
@@ -1,11 +1,16 @@
 package br.com.alura.forum.services;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.forum.models.Topico;
+import br.com.alura.forum.models.dtos.DetalhesDoTopicoDto;
+import br.com.alura.forum.models.dtos.TopicoDto;
 import br.com.alura.forum.models.forms.TopicoForm;
 import br.com.alura.forum.repositories.CursoRepository;
 import br.com.alura.forum.repositories.TopicoRepository;
@@ -31,5 +36,11 @@ public class TopicoService {
         Topico topico = form.converter(cursoRepository);
         repository.save(topico);
         return topico;
+    }
+
+    public DetalhesDoTopicoDto detalhar(Long id) {
+        Topico topico = repository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id, null));
+        return new DetalhesDoTopicoDto(topico);
     }
 }
