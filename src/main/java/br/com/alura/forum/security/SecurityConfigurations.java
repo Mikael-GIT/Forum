74000f777200b63955a4f38e162de20144b3b392
Mikael Tavares <mikael.tavares@unigranrio.br>
2021-11-09 03:37:08 -0300

    Adicionando configuracoes das rotas adicionais, que qualquer outra requisicao precisa estar autenticada

--- src/main/java/br/com/alura/forum/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/security/SecurityConfigurations.java
@@ -22,7 +22,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
-                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll().anyRequest().authenticated().and().formLogin();
     }
 
     // Configuracoes de recursos estaticos(requisicoes por arquivos javascript, css,
